ee1ae2ed4543cf71ea2236e59b758ee86bcaa3ce
GGG <gggkiller2@gmail.com>
2024-03-27 17:17:40 -0300

    Migrate to .NET 8.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Runtime.Versioning;
 using System.Windows.Forms;
 
 namespace MouseJiggler
 {
+    [SupportedOSPlatform("windows")]
     static class Program
     {
         /// <summary>
